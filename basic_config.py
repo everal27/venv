0848c7237e0b460b4c4b0af432faabae1c35e52c
Ever Contreras <contrerasever074@gmail.com>
2025-09-25 16:54:40 -0600

    actualizacion

--- basic_config.py
+++ basic_config.py
@@ -1,24 +1,23 @@
 import serial
 import time
 
-
-def configure_device(port, baurderate, hostname, username, password):
+def configure_device(port, baurderate, hostname, username, password, domain):
     try:
         ser = serial.Serial(port, baurderate, timeout=1)
-        ser.write(f"{port}\r\n".encode())
-        time.sleep(1) 
-        ser.write(f"eneale\r\n".encode())  
+        ser.write(f"en\r\n".encode())  
         time.sleep(1)
         ser.write(f"configure terminal\r\n".encode())
         time.sleep(1)
-        ser.write(f"hostame{hostname}\r\n".encode())
+        ser.write(f"hostname {hostname}\r\n".encode())
         time.sleep(1)
         ser.write(f"username {username} privilege 15 secret {password}\r\n".encode())
         time.sleep(1)
-        ser.write(f"ip domain-name example.com\r\n".encode())
+        ser.write(f"ip domain-name {domain}\r\n".encode())
         time.sleep(1)
-        ser.write(f"crypto key generate rsa 1024\r\n".encode())
-        time.sleep(1)
+        ser.write(b"crypto key generate rsa\r\n")
+        time.sleep(2)
+        ser.write(b"1024\r\n")
+        time.sleep(2)
         ser.write(f"line vty 0 4\r\n".encode())
         time.sleep(1)
         ser.write(f"login local\r\n".encode())
@@ -29,9 +28,11 @@ def configure_device(port, baurderate, hostname, username, password):
         time.sleep(1)
         ser.write(f"exit\r\n".encode()) 
         time.sleep(1)
-        ser.write(f"console line 0\r\n".encode())
+        ser.write(f"line console 0\r\n".encode())
         time.sleep(1)
         ser.write(f"login local\r\n".encode())
+        time.sleep(1)
+        ser.write(f"exit\r\n".encode())
         time.sleep(1)
         ser.write(f"exit\r\n".encode())
         time.sleep(1)
@@ -39,6 +40,5 @@ def configure_device(port, baurderate, hostname, username, password):
         time.sleep(1)
     except serial.SerialException as e:
         print(f"Error: {e}")
-        
-R1 = configure_device("COM5", 9600, "R1", "cisco", "cisco", "simon.com")
 
+R1 = configure_device("COM5", 9600, "R1", "cisco", "cisco", "simon.com")
